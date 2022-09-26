8820f0c994d98ac4044741723a53121cf08ec655
françois LE FORT <francois.lefort@zenika.com>
2022-09-26 10:50:46 +0200

    Add Google Map

--- app/src/main/java/com/flf69740/testwimova/ui/MainFragment.java
+++ app/src/main/java/com/flf69740/testwimova/ui/MainFragment.java
@@ -2,6 +2,7 @@ package com.flf69740.testwimova.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ViewModelProvider;
@@ -16,17 +17,25 @@ import com.flf69740.testwimova.R;
 import com.flf69740.testwimova.modele.MapPositions;
 import com.flf69740.testwimova.rx.Response;
 import com.flf69740.testwimova.viewmodel.MainViewModel;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import javax.annotation.Nullable;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
-public class MainFragment extends Fragment implements View.OnClickListener {
+public class MainFragment extends Fragment implements View.OnClickListener, OnMapReadyCallback {
 
     public MainFragment() {}
 
     private MainViewModel mainViewModel;
+
+    private GoogleMap mMap;
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -45,6 +54,11 @@ public class MainFragment extends Fragment implements View.OnClickListener {
 
         Button myBtn = view.findViewById(R.id.button_temp);
         myBtn.setOnClickListener(this);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null){
+            mapFragment.getMapAsync(this);
+        }
 
         return view;
     }
@@ -83,5 +97,21 @@ public class MainFragment extends Fragment implements View.OnClickListener {
 
     private void renderErrorState(Throwable throwable){
         Toast.makeText(getContext(), "ERROR", Toast.LENGTH_SHORT).show();
+    }
+
+    /*
+     * GOOGLE MAP
+     */
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
