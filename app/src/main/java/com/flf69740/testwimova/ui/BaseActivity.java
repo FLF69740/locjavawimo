4327ee5189649448beedcf0c73a1dd881ff599e4
françois LE FORT <francois.lefort@zenika.com>
2022-09-26 14:28:48 +0200

    Add Localization Permissions

--- app/src/main/java/com/flf69740/testwimova/ui/BaseActivity.java
+++ app/src/main/java/com/flf69740/testwimova/ui/BaseActivity.java
@@ -4,20 +4,26 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import javax.annotation.Nullable;
+
 public abstract class BaseActivity extends AppCompatActivity {
 
     protected abstract Fragment getFirstFragment();
     protected abstract int getContentView();
     protected abstract int getFragmentLayout();
+    protected abstract void getBody();
+
+    @Nullable Bundle bundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getContentView());
-        this.configureFragment(savedInstanceState);
+        bundle = savedInstanceState;
+        this.getBody();
     }
 
-    protected void configureFragment(Bundle bundle){
+    protected void configureFragment(){
         if (bundle == null){
             getSupportFragmentManager().beginTransaction()
                     .add(getFragmentLayout(), getFirstFragment())
--- app/src/main/java/com/flf69740/testwimova/ui/MainActivity.java
+++ app/src/main/java/com/flf69740/testwimova/ui/MainActivity.java
@@ -1,11 +1,20 @@
 package com.flf69740.testwimova.ui;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import com.flf69740.testwimova.R;
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 public class MainActivity extends BaseActivity {
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234321;
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
 
     @Override
     protected Fragment getFirstFragment() {
@@ -20,5 +29,32 @@ public class MainActivity extends BaseActivity {
     @Override
     protected int getFragmentLayout() {
         return R.id.frame_layout_main;
+    }
+
+    @Override
+    protected void getBody() {
+        enableMyLocation();
+    }
+
+    private void enableMyLocation(){
+        if (ContextCompat.checkSelfPermission(this, FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            String[] permissions = {FINE_LOCATION, COARSE_LOCATION};
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+            configureFragment();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            @NonNull String[] permissions,
+            @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            enableMyLocation();
+        }
     }
 }
